3d5d10e3f01890843a1acd03b00027251d7f060a
alanlewis764 <lewisa1@student.unimelb.edu.au>
2020-10-13 12:44:10 +1100

    naive space saving

--- SpaceSaving.cpp
+++ SpaceSaving.cpp
@@ -2,4 +2,40 @@
 // Created by Alan Lewis on 10/10/20.
 //
 
+#include <vector>
 #include "SpaceSaving.h"
+using namespace std;
+
+SpaceSaving::SpaceSaving(uint32_t t) : t(t) {}
+
+void SpaceSaving::update(uint64_t x) {
+    // if it is already being tracked then increment the counter
+    if (trackedItems.find(x) != trackedItems.end()) {
+        trackedItems[x]++;
+    } else if (trackedItems.size() < t) {
+        // add it to the tracked items with a count of 1
+        trackedItems[x] = 1;
+    } else {
+        // remove item with least count, c and add x with out c + 1
+        //TODO: this should be done in log(t) time. Either with additional
+        // BST/minHeap or some type of TreeMap
+        uint64_t minCount = UINT64_MAX;
+        uint64_t minItem;
+        for (auto &item: trackedItems) {
+            if (item.second < minCount) {
+                minItem = item.first;
+                minCount = item.second;
+            }
+        }
+        trackedItems.erase(minItem);
+        trackedItems[x] = minCount + 1;
+    }
+}
+
+vector<uint64_t> SpaceSaving::output() {
+    vector<uint64_t> ret;
+    for (const auto &item: trackedItems) {
+        ret.push_back(item.first);
+    }
+    return ret;
+}
--- SpaceSaving.h
+++ SpaceSaving.h
@@ -5,8 +5,18 @@
 #ifndef UNTITLED1_SPACESAVING_H
 #define UNTITLED1_SPACESAVING_H
 
+#include <cstdint>
+#include <unordered_map>
+
+
 
 class SpaceSaving {
+    uint32_t t;
+    std::unordered_map<uint64_t, uint64_t> trackedItems;
+public:
+    explicit SpaceSaving(uint32_t t);
+    void update(uint64_t x);
+    std::vector<uint64_t> output();
 
 };
 
